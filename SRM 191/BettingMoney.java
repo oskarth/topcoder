2423902f9de51fc6649c76d7db5c46f24c6250e4
oskarth <me@oskarth.com>
2015-03-31 01:18:01 +0200

    SRM 191: BettingMoney

---
+++ SRM 191/BettingMoney.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+public class BettingMoney {
+	public int moneyMade(int[] amounts, int[] centsPerDollar, int finalResult) {
+		return 0;
+	}
+
+// CUT begin
+	public static void main(String[] args){
+		System.err.println("BettingMoney (250 Points)");
+		System.err.println();
+		HashSet<Integer> cases = new HashSet<Integer>();
+        for (int i = 0; i < args.length; ++i) cases.add(Integer.parseInt(args[i]));
+        runTest(cases);
+	}
+
+	static void runTest(HashSet<Integer> caseSet) {
+	    int cases = 0, passed = 0;
+	    while (true) {
+	    	String label = Reader.nextLine();
+	    	if (label == null || !label.startsWith("--"))
+	    		break;
+
+            int[] amounts = new int[Integer.parseInt(Reader.nextLine())];
+            for (int i = 0; i < amounts.length; ++i)
+                amounts[i] = Integer.parseInt(Reader.nextLine());
+            int[] centsPerDollar = new int[Integer.parseInt(Reader.nextLine())];
+            for (int i = 0; i < centsPerDollar.length; ++i)
+                centsPerDollar[i] = Integer.parseInt(Reader.nextLine());
+            int finalResult = Integer.parseInt(Reader.nextLine());
+            Reader.nextLine();
+            int __answer = Integer.parseInt(Reader.nextLine());
+
+            cases++;
+            if (caseSet.size() > 0 && !caseSet.contains(cases - 1))
+                continue;
+    		System.err.print(String.format("  Testcase #%d ... ", cases - 1));
+
+            if (doTest(amounts, centsPerDollar, finalResult, __answer))
+                passed++;
+	    }
+	    if (caseSet.size() > 0) cases = caseSet.size();
+        System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
+
+        int T = (int)(System.currentTimeMillis() / 1000) - 1427756382;
+        double PT = T / 60.0, TT = 75.0;
+        System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
+	}
+
+	static boolean doTest(int[] amounts, int[] centsPerDollar, int finalResult, int __expected) {
+		long startTime = System.currentTimeMillis();
+		Throwable exception = null;
+		BettingMoney instance = new BettingMoney();
+		int __result = 0;
+		try {
+			__result = instance.moneyMade(amounts, centsPerDollar, finalResult);
+		}
+		catch (Throwable e) { exception = e; }
+		double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
+
+		if (exception != null) {
+			System.err.println("RUNTIME ERROR!");
+			exception.printStackTrace();
+			return false;
+		}
+		else if (__result == __expected) {
+			System.err.println("PASSED! " + String.format("(%.2f seconds)", elapsed));
+			return true;
+		}
+		else {
+			System.err.println("FAILED! " + String.format("(%.2f seconds)", elapsed));
+			System.err.println("           Expected: " + __expected);
+			System.err.println("           Received: " + __result);
+			return false;
+		}
+	}
+
+	static class Reader {
+        private static final String dataFileName = "BettingMoney.sample";
+	    private static BufferedReader reader;
+
+	    public static String nextLine() {
+	        try {
+                if (reader == null) {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(dataFileName)));
+                }
+                return reader.readLine();
+	        }
+	        catch (IOException e) {
+	            System.err.println("FATAL!! IOException");
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+	        return "";
+	    }
+	}
+// CUT end
+}
